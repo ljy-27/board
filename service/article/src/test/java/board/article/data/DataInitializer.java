732b6448e3095cfdcf39c716328c881a495db454
jaeyong <ljy3573057@gmail.com>
2025-07-28 21:37:42 +0900

    feat: 게시글 테스트 데이터 삽입

---
+++ service/article/src/test/java/board/article/data/DataInitializer.java
@@ -0,0 +1,61 @@
+package board.article.data;
+
+import board.article.entity.Article;
+import board.common.snowflake.Snowflake;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@SpringBootTest
+@ActiveProfiles("dev")
+public class DataInitializer {
+
+    static final int BULK_INSERT_SIZE = 2000;
+    static final int EXECUTE_COUNT = 6000;
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Autowired
+    TransactionTemplate tx;
+
+    Snowflake snowflake = new Snowflake();
+    CountDownLatch latch = new CountDownLatch(EXECUTE_COUNT);
+
+    @Test
+    void initialize() throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < EXECUTE_COUNT; i++) {
+            executorService.submit(() -> {
+                insert();
+                latch.countDown();
+                System.out.println("latch count: " + latch.getCount());
+            });
+        }
+        latch.await();
+        executorService.shutdown();
+    }
+
+    void insert() {
+        tx.executeWithoutResult(status -> {
+            for (int i = 0; i < BULK_INSERT_SIZE; i++) {
+                Article article = Article.of(
+                        snowflake.nextId(),
+                        "title" + i,
+                        "content" + i,
+                        1L,
+                        1L
+                );
+                em.persist(article);
+            }
+        });
+    }
+}
